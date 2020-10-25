33c95c3344d28a3b87c5bd2453481178651391db
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-10-25 12:40:47 -0300

    Update main.c

--- main.c
+++ main.c
@@ -31,41 +31,40 @@
 
 /* ------------------------ Bits de configuración --------------------------- */
 // CONFIG1
-#pragma config FOSC = XT           // Oscillator Selection bits (XT oscillator: Crystal/resonator on RA6/OSC2/CLKOUT and RA7/OSC1/CLKIN)
-//#pragma config FOSC = INTRC_NOCLKOUT // Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
-#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
-#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
-#pragma config MCLRE = ON       // RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
-#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
-#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
-#pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
-#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
-#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
-#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+#pragma config FOSC = XT        // Oscillator Selection bits
+//#pragma config FOSC = INTRC_NOCLKOUT // Oscillator Selection bits
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit
+#pragma config MCLRE = ON       // RE3/MCLR pin function select bit
+#pragma config CP = OFF         // Code Protection bit
+#pragma config CPD = OFF        // Data Code Protection bit
+#pragma config BOREN = OFF      // Brown Out Reset Selection bits
+#pragma config IESO = OFF       // Internal External Switchover bit
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit
 
 // CONFIG2
-#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
-#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits
 
 /* ------------------------ Prototipos de funciones ------------------------- */
 void gpio_config();
 
 /* ------------------------ Implementación de funciones --------------------- */
-void main(void) {               // Función principal
-    gpio_config();              // Inicializo las entradas y salidas
+void main(void) {                       // Función principal
+    gpio_config();                      // Inicializo las entradas y salidas
 
-    PIN_LED1 = 0;               // Apago el LED1
+    PIN_LED1 = 0;                       // Apago el LED1
     
-    while(1) {                  // Super loop
-        if( PIN_TEC1 == 0 ) {   // Espero que se presione la TEC1
-            __delay_ms(40);
+    while(1) {  // Super loop, https://pbs.twimg.com/media/BafQje7CcAAN5en.jpg
+        if( PIN_TEC1 == 0 ) {           // Espero que se presione la TEC1
+            __delay_ms(40);             // Delay antirrebote
+            
             PIN_LED1 = !PIN_LED1;       // Enciendo el LED1
 
-            while( PIN_TEC1 == 0 );
-            __delay_ms(40);
+            while( PIN_TEC1 == 0 );     // Espero que se suelte la TEC1
+            __delay_ms(40);             // Delay antirrebote
         }
-
-        __delay_ms(10);
     }
     
     return;
